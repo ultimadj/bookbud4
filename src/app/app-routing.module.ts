7fa603f5ed729bd4fab2c1ba722762f4e084b784
David Johnson <ultimadj@gmail.com>
2017-02-12 22:27:18 -0500

    Better nav direction

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import {BookComponent} from "./book/book.component";
 import {LoginComponent} from "./login/login.component";
 import {LandingComponent} from "./landing/landing.component";
 const routes: Routes = [
-  { path: '', component: LandingComponent, pathMatch: 'full' },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {path: 'dashboard', component: LandingComponent},
   { path: 'book',  component: BookComponent },
   { path: 'login', component: LoginComponent }
 ];
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import {Component} from '@angular/core';
 import {AngularFire} from 'angularfire2';
+import {UserService} from "./user.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  display = {};
+
+  constructor(public userService: UserService) {
+    this.display = {loading: true};
+    userService.user.subscribe((userDetail) => {
+      if(userDetail) {
+        this.display = {showLogout: true};
+      } else {
+        this.display = {showLogin: true};
+      }
+    });
+  }
+
+  login() {
+    this.userService.login();
+  }
+  logout() {
+    this.userService.logout();
+  }
 }
